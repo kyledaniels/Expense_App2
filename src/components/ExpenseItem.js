cb3d96cf528cc32bb9834c37c6c3ac4803e27267
kyle67 <daniels.kyle@gmail.com>
2021-10-03 12:16:52 -0400

    added css to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,16 @@
+import './ExpenseItem.css'
+
 function ExpenseItem () {
-    return <h2>Expense</h2>
-}
+    return (
+        <div className="expense-item">
+            <div>March 28th 20201</div>
+            <div className="expense-item__description">
+                <h2>Car Insurance</h2>
+                <div className="expense-item__price">$294.67</div>
+            </div>
+        </div>
+      );
+    }  
 
 
 
